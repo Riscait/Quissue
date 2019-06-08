896665bf9d54011fdce80b86a3200491faf79735
Riscait <mr@nerco.jp>
2019-06-08 14:51:41 +0900

    Initial Commit

---
+++ Quissue/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Quissue
+//
+//  Created by 村松龍之介 on 2019/06/08.
+//  Copyright © 2019 村松龍之介. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
